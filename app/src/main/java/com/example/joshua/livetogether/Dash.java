55f18da74b26f2816959273d083b227f6d614d8a
Joshua van Gogh <jsvangogh@gmail.com>
2015-10-27 21:54:58 -0700

    File not found exception when creating BufferedReader

--- app/src/main/java/com/example/joshua/livetogether/Dash.java
+++ app/src/main/java/com/example/joshua/livetogether/Dash.java
@@ -1,9 +1,11 @@
 package com.example.joshua.livetogether;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,6 +16,8 @@ public class Dash extends AppCompatActivity {
 
     private String aptID = null;
     ArrayList<String> tasks;
+    TaskRetriever mTaskRetriever;
+    TextView mTasksView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,50 +31,104 @@ public class Dash extends AppCompatActivity {
 
         // initialize the task array list
         tasks = new ArrayList<>();
+
+        mTasksView = (TextView) findViewById(R.id.Tasks);
     }
 
     // Update the task list on resume
     @Override
-    protected void onResume()
-    {
-
+    protected void onResume() {
         super.onResume();
-        String taskList = ServerCom.getTasks(aptID);
-        String[] tempTasks;
-        if(taskList != null) {
-            tempTasks = taskList.split("*");
-        }
-        else
-        {
-            tempTasks = new String[]{"FUUUDGE"};
-        }
-        tasks = new ArrayList<>();
-
-        for(String a: tempTasks)
-        {
-            tasks.add(a);
-        }
-
-        Iterator<String> taskIterator = tasks.iterator();
-        String s = "";
-
-        for(int i = 0; i < tasks.size(); i++)
-        {
-            if(taskIterator.hasNext())
-            {
-                s += taskIterator.next();
-                s += "\n";
-            }
-        }
-
-        TextView tasksView = (TextView) findViewById(R.id.Tasks);
-        tasksView.setText(s);
+        String taskList = null;
+        mTaskRetriever = new TaskRetriever(aptID);
+        mTaskRetriever.execute((Void) null);
+//        String[] tempTasks;
+//        if(taskList != null) {
+//            tempTasks = taskList.split("*");
+//        }
+//        else
+//        {
+//            tempTasks = new String[]{"FUUUDGE"};
+//        }
+//        tasks = new ArrayList<>();
+//
+//        for(String a: tempTasks)
+//        {
+//            tasks.add(a);
+//        }
+//
+//        Iterator<String> taskIterator = tasks.iterator();
+//        String s = "";
+//
+//        for(int i = 0; i < tasks.size(); i++)
+//        {
+//            if(taskIterator.hasNext())
+//            {
+//                s += taskIterator.next();
+//                s += "\n";
+//            }
+//        }
+//
+//        TextView tasksView = (TextView) findViewById(R.id.Tasks);
+//        tasksView.setText(s);
     }
 
-    public void add(View view)
-    {
+    public void add(View view) {
         Intent addIntent = new Intent(this, AddTask.class);
         addIntent.putExtra("com.example.joshua.livetogether.aptID", "56306f75129270000ac80798");
         startActivity(addIntent);
+    }
+
+
+    class TaskRetriever extends AsyncTask<Void, Void, String> {
+        String maptID;
+        String mtaskString;
+        Exception exception;
+
+
+        TaskRetriever(String aptID) {
+            maptID = aptID;
+        }
+
+        @Override
+        protected String doInBackground(Void... v) {
+
+            try {
+                mtaskString = ServerCom.getTasks(maptID);
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+
+            return mtaskString;
+        }
+
+        protected void onPostExecute(String taskString) {
+            String[] tempTasks;
+            if (mtaskString != null) {
+                tempTasks = mtaskString.split("*");
+            } else {
+                tempTasks = new String[]{"FUUUDGE"};
+            }
+            tasks = new ArrayList<>();
+
+            for (String a : tempTasks) {
+                tasks.add(a);
+            }
+
+            Iterator<String> taskIterator = tasks.iterator();
+            String s = "";
+
+            for (int i = 0; i < tasks.size(); i++) {
+                if (taskIterator.hasNext()) {
+                    s += taskIterator.next();
+                    s += "\n";
+                }
+            }
+
+            mTasksView.setText(s);
+
+            mTaskRetriever = null;
+        }
     }
 }
