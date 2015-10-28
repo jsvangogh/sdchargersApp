5b8be2e22dfdbc0fd01d47193bea05a5970b957d
Alan Chen <azc003@ucsd.edu>
2015-10-27 23:07:56 -0700

    forgot some of the files

---
+++ app/src/main/java/com/example/joshua/livetogether/AddTask.java
@@ -0,0 +1,49 @@
+package com.example.joshua.livetogether;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.KeyEvent;
+
+public class AddTask extends AppCompatActivity {
+
+    private boolean notif = false;
+    private String task = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void submitForm(View view) {
+        final EditText editTextName = (EditText) findViewById(R.id.editText);
+        editTextName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                task = editTextName.getText().toString();
+                //addTask(task);
+                finish();
+                return true;
+            }
+        });
+    }
+
+
+}
