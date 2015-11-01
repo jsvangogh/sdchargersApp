914bd84524b399c9f5bfc8fa61b3f6a9e69468f2
Alan Chen <azc003@ucsd.edu>
2015-10-31 23:16:17 -0700

    Updated Dash with Alert Dialog Capability

--- app/src/main/java/com/example/joshua/livetogether/Dash.java
+++ app/src/main/java/com/example/joshua/livetogether/Dash.java
@@ -1,16 +1,23 @@
 package com.example.joshua.livetogether;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.view.LayoutInflater;
 
 public class Dash extends AppCompatActivity {
 
     private String maptID = null;
+    private String mUserID = null;
     TaskRetriever mTaskRetriever;
+    ApartmentRetriever mApartmentRetriever;
     TextView mTasksView;
 
     @Override
@@ -18,10 +25,18 @@ public class Dash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_dash);
-
+        String aptName = null;
         // get the apartment ID from the login page
         Intent intent = getIntent();
-        maptID = intent.getStringExtra("com.example.joshua.livetogether.aptID");
+        mUserID = intent.getStringExtra("com.example.joshua.livetogether.user");
+        mApartmentRetriever = new ApartmentRetriever(aptName);
+        mApartmentRetriever.execute((Void) null);
+
+        while(maptID == null) {
+            mApartmentRetriever = new ApartmentRetriever(aptName);
+            mApartmentRetriever.execute((Void) null);
+            aptName = showInputDialog();
+        }
 
         mTasksView = (TextView) findViewById(R.id.Tasks);
     }
@@ -40,6 +55,33 @@ public class Dash extends AppCompatActivity {
         Intent addIntent = new Intent(this, AddTask.class);
         addIntent.putExtra("com.example.joshua.livetogether.aptID", "56306f75129270000ac80798");
         startActivity(addIntent);
+    }
+
+    public String showInputDialog() {
+        String name = null;
+        LayoutInflater layoutInflater = LayoutInflater.from(Dash.this);
+        View view  = layoutInflater.inflate(R.layout.input_dialog, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(Dash.this);
+        builder.setTitle("No Apartment!");
+        builder.setMessage("No apartment associated with user! Please enter an apartment" +
+                " name to create an apartmnet");
+        builder.setView(view);
+        final EditText input = (EditText) view.findViewById(R.id.editText10);
+        //input.setInputType(InputType.TYPE_CLASS_TEXT);
+        //builder.setView(input);
+
+        while(name == null) {
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    name = input.getText().toString();
+                }
+            });
+        }
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        return name;
     }
 
 
@@ -75,4 +117,40 @@ public class Dash extends AppCompatActivity {
             mTaskRetriever = null;
         }
     }
+
+
+    class ApartmentRetriever extends AsyncTask<Void, Void, Void> {
+        private String maptName = null;
+        //String mAptID;
+        Exception exception;
+
+        ApartmentRetriever(String aptName)
+        {
+            maptName = aptName;
+        }
+
+        @Override
+        protected Void doInBackground(Void... v) {
+
+            if(maptName == null) {
+                try {
+                    maptID = ServerCom.getAparmentID(mUserID);
+                } catch (Exception e) {
+                    this.exception = e;
+                }
+            }
+            else{
+                try {
+                    ServerCom.setApartmentID(mUserID, maptName);
+                    maptID = ServerCom.getApartmentID(mUserID);
+                } catch (Exception e) {
+                    this.exception = e;
+                }
+            }
+
+            return null;
+        }
+
+    }
+
 }
