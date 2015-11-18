3f8c1580f594b6d0ccd93e9decbee87c8a5ae1cd
Alan Chen <azc003@ucsd.edu>
2015-11-17 23:29:08 -0800

    Updated toolbar with buttons

--- app/src/main/java/com/example/joshua/livetogether/Dash.java
+++ app/src/main/java/com/example/joshua/livetogether/Dash.java
@@ -61,6 +61,14 @@ public class Dash extends AppCompatActivity {
         startActivity(addIntent);
     }
 
+    public void allTasks(View view) {
+
+    }
+
+    public void myTasks(View view) {
+
+    }
+
     class TaskRetriever extends AsyncTask<Void, Void, Void> {
         Task tempTasks[];
         Exception exception;
